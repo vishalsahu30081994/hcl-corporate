a7618be80fac432b7dd5cefbfea4af7f90ee42f3
vishalsahu30081994 <50576178+vishalsahu30081994@users.noreply.github.com>
2019-09-11 00:20:53 +0530

    My first-react-host

---
+++ precache-manifest.dfefee9ee042887a4e9c039226794f10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc59e57b98577cb4528389431ebd07f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675ba50a0c9bbd0c22a0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "609c38494f98f0c2d3fd",
+    "url": "/static/js/2.06aff84f.chunk.js"
+  },
+  {
+    "revision": "675ba50a0c9bbd0c22a0",
+    "url": "/static/js/main.51d93126.chunk.js"
+  },
+  {
+    "revision": "1f324d4b8b1d1b021e87",
+    "url": "/static/js/runtime~main.2c7ae24b.js"
+  }
+]);
